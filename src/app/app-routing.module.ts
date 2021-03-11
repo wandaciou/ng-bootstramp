a5c28399ef29fe8b1b7153cf80d2e431a92a3821
wanting <wandaciou@gmail.com>
2021-03-11 17:45:41 +0800

    install ng-bootstrap, create component, set routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TablePageComponent } from './component/table-page/table-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'table-page', component: TablePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
